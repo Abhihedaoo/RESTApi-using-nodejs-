7bdf3eb148169a6b93ed59683f3469cba632aa12
Abhishek Hedaoo <abhihedaoo98@gmail.com>
2020-09-20 12:44:16 +0530

    CRUD operation with middleware

---
+++ readme.md
@@ -0,0 +1,13 @@
+# RestAPI using nodejs
+
+This repository is for to use the nodejs to create the restful api step by step. Every commit will contain a new feature
+
+## Getting started
+
+Just clone the repository and checkout to different commit for different features
+
+### Prerequisites
+
+Mongodb installed in your system
+latest node must be installed
+
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+require("dotenv").config()
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-    console.log("Server has started the port 3000")
-})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on("error", error => console.log(error));
+db.once("open", () => console.log("Connection to db is established"));
+
+app.use(express.json())
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter)
+
+app.listen(process.env.PORT, () => console.log(`server has started at port ${process.env.PORT}`));
